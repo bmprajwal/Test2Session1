0d107904db3d503aaa402a482b2701d47d933235
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-02-23 12:39:18 +0000

    .

---
+++ p3original.c
@@ -0,0 +1,36 @@
+#include <stdio.h> 
+
+int input_number();
+int is_prime(int n);
+void output(int n, int is_prime);
+
+int main(){
+  int n = input_number();
+  int isprime = is_prime(n);
+  output(n, isprime);
+
+  return 0;
+}
+int input_number(){
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  return n;
+}
+int is_prime(int n){
+  int count = 0;
+  if(n==1)
+    return 1;
+  else 
+    for(int i=1; i<=n;i++){
+      if(n%i==0)
+      count++;
+    }
+   return count;
+}
+void output(int n, int isprime){
+  if(isprime==1 || isprime<=2)
+    printf("%d is a prime number.\n",n);
+  else
+    printf("%d is not a prime number.\n",n);
+}
